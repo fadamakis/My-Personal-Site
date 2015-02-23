ef3da96d2ca22847b5082dd12bc8c787f80aae32
Developer 3 <dev3@army.gr>
2015-02-23 12:59:48 +0200

    Material resources

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){        
+    $('#intro').addClass('faded');
+    $('#intro h1, #intro h2, #intro .btn').addClass('animated zoomIn');
+    $('nav').addClass('animated fadeInDown');
+    setInterval(function(){
+    	$('#intro h2 span').addClass('animated bounce');
+    }, 2000);
+});
