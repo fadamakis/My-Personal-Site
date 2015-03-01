a50b7e20d9428fb4c961aee02e7523d25963b6d5
Fotis Adamakis <fotis316@yahoo.com>
2015-03-01 12:00:04 +0200

    Section fade in effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){        
     $('#intro').addClass('faded');
     $('#intro h1, #intro h2, #intro .btn').addClass('animated zoomIn');
-    $('.row.pull').addClass('animated fadeInUp');
+    $('section').addClass('animated fadeIn');
     $('nav').addClass('animated fadeInDown');
     setInterval(function(){
     	$('#intro h2 span').addClass('animated bounce');
