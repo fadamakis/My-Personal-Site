74490b83305bb3f2648bdf1f897dc1efaf2ef1f9
Fotis Adamakis <fotis316@yahoo.com>
2015-03-02 11:49:59 +0200

    Responsive fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){        
     $('#intro').addClass('faded');
     $('#intro h1, #intro h2, #intro .btn').addClass('animated zoomIn');
-    $('section').addClass('animated fadeIn');
+    $('body').addClass('animated fadeIn');
     $('nav').addClass('animated fadeInDown');
     setInterval(function(){
     	$('#intro h2 span').addClass('animated bounce');
