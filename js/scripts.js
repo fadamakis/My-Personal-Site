a2e0b1b70bc403ad26e32b7808c9d85d1fd7d086
Developer 3 <dev3@army.gr>
2015-03-05 22:12:37 +0200

    Mobile opacity removed

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('#intro').addClass('faded');
     $('#intro h1, #intro h2, #intro .btn').addClass('animated zoomIn');
     $('body').addClass('animated fadeIn');
-    $('nav').addClass('animated fadeInDown');
     setInterval(function(){
     	$('#intro h2 span').addClass('animated bounce');
     }, 2000);
