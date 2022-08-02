d5049bf63182ef1ae50d819c8117bd375b8caab7
Fotis Adamakis <fotis316@yahoo.com>
2022-08-02 21:37:57 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#intro').addClass('faded');
     $('#intro h1, #intro h2, #intro .btn').addClass('animated zoomIn');
     $('nav').addClass('animated fadeInDown');
-    setInterval(function(){
+    setTimeout(function(){
     	$('#intro h2 span').addClass('animated bounce');
     }, 2000);
 
